6d9dad2c499247b98744e550cc0c6de4a91a5c53
酒武灵 <1033766950@qq.com>
2020-05-07 16:35:45 +0800

    添加布局文件

---
+++ src/main/java/org/note/HomeController.java
@@ -0,0 +1,15 @@
+package org.note;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+
+public class HomeController {
+
+    @FXML
+    private MenuBar homeMenu;
+
+    public void initialize() {
+
+    }
+}
--- src/main/java/org/note/MainApp.java
+++ src/main/java/org/note/MainApp.java
@@ -6,17 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        Parent root = FXMLLoader.load(getFxml("home.fxml"));
 
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        scene.getStylesheets().add(getCss("styles.css"));
 
-        stage.setTitle("JavaFX and Gradle");
+        stage.setTitle("The Note");
         stage.setScene(scene);
         stage.show();
     }
@@ -25,4 +27,11 @@ public class MainApp extends Application {
         launch(args);
     }
 
+    private URL getFxml(String name) {
+        return getClass().getResource("/fxml/" + name);
+    }
+
+    private String getCss(String name) {
+        return getClass().getResource("/css/" + name).toExternalForm();
+    }
 }
